b4f30cfbd5ef0caeaa1134dde3a38698c3e66b6f
Gautier Michelin <gm@ideesculture.com>
2019-05-26 11:15:17 +0200

    fetching config, form is live from conf

--- views/index_html.php
+++ views/index_html.php
@@ -1,5 +1,8 @@
 <?php
 	$id = $this->getVar("object_id");
+    $template = $this->getVar("template");
+    $mappings = $this->getVar("mappings");
+
 	error_reporting(E_ERROR);
 	?>
 <h1>Contribute to the database</h1>
@@ -14,52 +17,24 @@
 
 <div id="form1" style="padding: 2px 2px 90px 2px"></div>
 <script type="text/javascript">
-$("#form1").alpaca({ 
-	"data": {
-		 "title": "ca_objects.preferred_labels",
-		 "isbn": "ca_objects.isbn_grandjeu",
-		 "year": "ca_objects.date",
-		 "alleged_date": "ca_objects.date_presumee",
-		 "pages": "ca_objects.pages",
-		 "alleged_page_amount": "ca_objects.pages_alleged", 
-		 "language": "ca_objects.lang",	 
-		 "weight": "ca_objects.weight",
-		 "keywords": "ca_objects.keywords",
-		 "description": "ca_objects.notes", 
-         "edition": "ca_objects.edition",
-         "identifier":"ca_objects.idno" , 
-         "credits": "ca_object.credits",
-         "authors": "ca_entities.preferred_labels",
-         "publishers": "ca_entities.preferred_labels",
-         "related_collections":  "ca_collections"	 
-	},
+$("#form1").alpaca({
+    "data": {
+<?php
+foreach($mappings as $field=>$properties) {
+    print "\t\t\"".$field."\": \"".$properties["default"]."\",\n";
+}
+?>
+    },
     "schema": {
         "type": "object",
         "properties": {
-            "title": {
-                "type": "string",
-                "title": "Title"
-            },
-            "isbn": {
-                "type": "string",
-                "title": "ISBN"
-            },
-            "year": {
-                "type": "string",
-                "title": "Year"
-            },
-            "alleged_date": {
-                "type": "string",
-                "title": "Alleged Date"
-            },
-            "pages": {
-                "type": "string",
-                "title": "Pages"
-            },
-            "alleged_page_amount": {
-                "type": "string",
-                "title": "Alleged page amount"
-            },
+<?php foreach($mappings as $field=>$properties) :
+print "\t\t\t'{$field}' : {\n";
+foreach($properties as $name=>$property) {
+    if(is_string($property) && ($name != "mapping")) print "\t\t\t\t\"{$name}\": \"{$property}\",\n";
+}
+print "\t\t\t},\n";
+            endforeach; ?>
             "language": {
                 "type": "select",
                 "title": "Language",
@@ -83,63 +58,23 @@ $("#form1").alpaca({
                     }
                 }
             },
-            "weight": {
-                "type": "string",
-                "title": "Weight"
-            },
-            "keywords": {
-                "type": "string",
-                "title": "Keywords"
-            },
-            "description": {
-                "type": "string",
-                "title": "Description"
-            },
-            "edition": {
-                "type": "string",
-                "title": "Edition"
-            },
-            "identifier": {
-                "type": "string",
-                "title": "Object identifier"
-            },
-            "credits": {
-                "type": "string",
-                "title": "Credits"
-            },
-            "authors": {
-                "type": "string",
-                "title": "Author(s)"
-            },
-            "publishers": {
-                "type": "string",
-                "title": "Publisher(s)"
-            },
-            "related_collections": {
-                "type": "string",
-                "title": "Related collections"
-            }       
         }
     },
-     "options": {
-        "fields": {
-            "alleged_date": { "type": "checkbox" },
-            "alleged_page_amount": {"type": "checkbox"}
-        },
-        "attributes": {
-            "action": "http://httpbin.org/post",
-            "method": "post"
-        },
+    "options": {
         "form": {
-        	"buttons": {
-            	"submit": {
-	            	"title": "Contribute",
-					"type": "button",
-	            	"styles": "btn btn-primary"
-            	}
-        	}
+            "attributes": {
+                "action": "<?php print __CA_URL_ROOT__; ?>/index.php/Contribuer/Do/Create",
+                "method": "post"
+            },
+            "buttons": {
+                "submit": {
+                    "title":"Valider"
+                }
+            }
+        },
+        "fields": {
         }
-    } 
- 
+    },
+    "view": "bootstrap-edit"
 });
 </script>
