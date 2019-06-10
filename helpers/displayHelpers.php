952c4f7fff3d1d331a4e8a88be5d8e42b2a1c7f2
Gautier Michelin <gm@ideesculture.com>
2019-06-10 18:00:06 +0200

    preparing plugin displayHelpers functions

---
+++ helpers/displayHelpers.php
@@ -0,0 +1,11 @@
+<?php
+
+function DeleteButton() {
+    print "<button>Delete</button>";
+    return true;
+}
+
+function AddMediaButton() {
+    print "<button>Add media</button>";
+    return true;
+}
