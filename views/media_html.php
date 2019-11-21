1f04f8e0ee3adad1c90fe0700582bf33f7fdda26
Gautier MICHELIN <gm@ideesculture.com>
2019-11-21 14:51:15 +0100

    media

--- views/media_html.php
+++ views/media_html.php
@@ -1,23 +1,26 @@
 <?php
 $id = $this->getVar("id");
 ?>
-<script type="text/javascript" src="<?php print __CA_URL_ROOT__; ?>/app/plugins/Contribuer/lib/dropzone.js"></script>
-<link type="text/css" href="<?php print __CA_URL_ROOT__; ?>/app/plugins/Contribuer/lib/dropzone.css" rel="stylesheet"/>
-<form method="POST" action="<?php print __CA_URL_ROOT__ ?>/index.php/Contribuer/Do/PostMedia/id/<?php print $id ?>" class="dropzone needsclick dz-clickable" id="demo-upload">
+<div style='padding-top:120px' class='container'>
+	<div class='row'>
+		<div class='col-md-12'>
+			<script type="text/javascript" src="<?php print __CA_URL_ROOT__; ?>/app/plugins/Contribuer/lib/dropzone.js"></script>
+			<link type="text/css" href="<?php print __CA_URL_ROOT__; ?>/app/plugins/Contribuer/lib/dropzone.css" rel="stylesheet"/>
+			<form method="POST" action="<?php print __CA_URL_ROOT__ ?>/index.php/Contribuer/Do/PostMedia/id/<?php print $id ?>" class="dropzone needsclick dz-clickable" id="demo-upload">
 
-    <div class="dz-message needsclick">
-        <span class="glyphicon glyphicon-file"></span>
-        Glisser ici les médias<br/>à attacher à la fiche
-    </div>
-</form>
-<a href="<?php print __CA_URL_ROOT__; ?>/index.php/Detail/objects/<?php print $id; ?>" class="backlink"><i class="fa fa-angle-double-left"></i> Retourner sur la fiche</a>
+			    <div class="dz-message needsclick">
+			        <span class="glyphicon glyphicon-file"></span>
+			        Drop here the medias to <br/>attach to this record
+			    </div>
+			</form>
+			<a href="<?php print __CA_URL_ROOT__; ?>/index.php/Detail/objects/<?php print $id; ?>" class="backlink"><i class="fa fa-angle-double-left"></i> Back</a>
 <style>
     #dz, .dropzone {
         border:1px dashed lightblue;
         font-size:22px;
     }
     #dz .glyphicon, .dropzone .glyphicon {
-        color:rgba(33, 186, 206, 1.00);
+        color:#eeba3f;
     }
     .backlink {
         color: #999;
