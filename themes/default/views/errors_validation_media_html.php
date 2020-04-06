7403e09568c0aac8c330e1e3edfa9e6c63295e64
Rachel <ro@ideesculture.com>
2020-04-06 10:35:53 +0200

    comment on error feedback

--- themes/default/views/errors_validation_media_html.php
+++ themes/default/views/errors_validation_media_html.php
@@ -6,6 +6,7 @@
 <div class="container" style="padding-top:120px;">
     <div class="row">
         <div class="col-md-12">
+            <!-- Suggestion Rachel : An issue was found on this image input -->
             <h1>Problem with this image contribution</h1>
             <p>Your image contribution could not be added to the database, please contact the administrators with your image and the following error message.</p>
 			<p><?php print $errors; ?></p>
